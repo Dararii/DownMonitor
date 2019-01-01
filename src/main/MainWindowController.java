f4bf43e9a68f8c5b5fdb7b01794fc882c37c539c
Darari <darari7@gmail.com>
2019-01-01 09:07:29 +0700

    add timer engine and TextArea function to table view

--- src/main/MainWindowController.java
+++ src/main/MainWindowController.java
@@ -1,18 +1,17 @@
 package main;
 
 import com.healthmarketscience.jackcess.DatabaseBuilder;
-import javafx.beans.property.SimpleObjectProperty;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.util.Callback;
 import javafx.util.converter.DefaultStringConverter;
 
 import java.io.File;
@@ -22,6 +21,9 @@ import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.ResourceBundle;
 
@@ -65,6 +67,18 @@ public class MainWindowController implements Initializable {
     ObservableList<String> defectType = FXCollections.observableArrayList();
     ObservableList<String> shiftType = FXCollections.observableArrayList();
 
+    Calendar currentDateTime = Calendar.getInstance();
+    LocalDateTime currentCounterTime;
+    Timeline clock = new Timeline(new KeyFrame(javafx.util.Duration.ZERO, e -> {
+        currentCounterTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        textTime.setText(currentCounterTime.format(formatter));
+        textDate.setText(currentCounterTime.format(formatter2));
+    }),
+            new KeyFrame(javafx.util.Duration.seconds(1))
+    );
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         path = Engine.GetCurrentDir() + "\\database.accdb";
@@ -87,6 +101,8 @@ public class MainWindowController implements Initializable {
         }
 
         //init data
+        currentDateTime.setTimeInMillis(System.currentTimeMillis());
+
         //cbSelectPort
         shiftType.addAll("A", "B");
         defectType.addAll("A", "B", "C", "D");
@@ -96,6 +112,17 @@ public class MainWindowController implements Initializable {
         dt.add(new DownTime(2, 4, 6, "B", "HAHAHA"));
 
         initTableDownTime();
+    }
+
+    @FXML
+    public void startTimer() {
+        clock.setCycleCount(Animation.INDEFINITE);
+        clock.play();
+    }
+
+    @FXML
+    public void stopTimer() {
+        clock.stop();
     }
 
     private void initTableDownTime() {
@@ -109,6 +136,7 @@ public class MainWindowController implements Initializable {
             property.setValue(dateFormat.format(time));
             return property;
         });
+
         colDuration.setCellValueFactory(downTime -> {
             if (downTime.getValue().getDurationInSecond() == 0)
                 return null;
@@ -117,11 +145,19 @@ public class MainWindowController implements Initializable {
             property.setValue(formatDuration(dur));
             return property;
         });
+
         colDefect.setCellValueFactory(new PropertyValueFactory<>("defect"));
         colDefect.setCellFactory(ComboBoxTableCell.forTableColumn(new DefaultStringConverter(), defectType));
         colDefect.setOnEditCommit(event -> {
             event.getRowValue().setDefect(event.getNewValue());
             System.out.println(dt.get(0).getDefect());
+        });
+
+        colReason.setCellValueFactory(new PropertyValueFactory<>("reason"));
+        colReason.setCellFactory(TextAreaTableCell.forTableColumn(new DefaultStringConverter()));
+        colReason.setOnEditCommit(event -> {
+            event.getRowValue().setReason(event.getNewValue());
+            System.out.println(dt.get(0).getReason());
         });
 
         listData.setEditable(true);
